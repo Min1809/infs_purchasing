aaec8b39296e20f76031a7b006a71dac537033e2
Min1809 <min@infinityitsuccess.com>
2025-11-27 11:08:47 +0700

    rfq group approval mail fix

--- models/purchasegroup.py
+++ models/purchasegroup.py
@@ -16,6 +16,8 @@ class PurchaseOrderGroupInherit(models.Model):
         # ('confirmed', 'Confirmed'),
         ('canceled', 'Canceled'),
     ], string='Approval Stage', default='draft', tracking=True)
+    
+    approval_status = fields.Char(string='Approval Status', compute='_compute_approval_status')
 
     name = fields.Char(string='Group Reference', required=True, copy=False, readonly=True,
                        default='New')
@@ -24,6 +26,11 @@ class PurchaseOrderGroupInherit(models.Model):
     
     has_supervisor_2 = fields.Boolean(string='Has Level 2 Supervisor', compute='_compute_has_supervisor_2', default=False)
     has_supervisor_3 = fields.Boolean(string='Has Level 3 Supervisor', compute='_compute_has_supervisor_3', default=False)
+
+    @api.depends('approval_stage')
+    def _compute_approval_status(self):
+        for order in self:
+            order.approval_status = dict(self.fields_get(allfields=['approval_stage'])['approval_stage']['selection']).get(order.approval_stage, 'Unknown')
 
     def write(self, vals):
         """
@@ -89,13 +96,13 @@ class PurchaseOrderGroupInherit(models.Model):
                 vals['name'] = self.env['ir.sequence'].next_by_code('purchase.order.group') or 'New'
         return super(PurchaseOrderGroupInherit, self).create(vals_list)
     
-    def get_rfqgp_url(self, company):
+    def get_rfqgp_url(self):
         base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
-        return f"{base_url}/web#id={self.id}&model=purchase.order.group&view_type=form&cids={company.id}"
+        return f"{base_url}/web#id={self.id}&cids=1-2&menu_id=614&action=916&model=purchase.order.group&view_type=form"
 
-    def get_ip_rfqgp_url(self, company):
+    def get_ip_rfqgp_url(self):
         web_ip_url = self.env['ir.config_parameter'].sudo().get_param('website.ip_address')
-        return f"{web_ip_url}/web#id={self.id}&model=purchase.order.group&view_type=form&cids={company.id}"
+        return f"{web_ip_url}/web#id={self.id}&cids=1-2&menu_id=614&action=916&model=purchase.order.group&view_type=form"
     
     def action_compare_order_lines(self):
         """Open a view to compare all order lines from purchase orders in this group"""
@@ -166,22 +173,57 @@ class PurchaseOrderGroupInherit(models.Model):
         return status
 
     def _send_approval_notification(self):
-        stage = self.approval_stage
+        for record in self:
+            stage = record.approval_stage
+            send_approval_mails = record.check_approval_mail_status()
+            _logger.info(f"send_approval_mails: {send_approval_mails}")
+            if send_approval_mails:
+                _logger.info(f"Sending approval notification for stage: {stage}")
+                if stage == 'submitted':
+                    record._send_approval_email('infs_purchasing.email_template_rfqgp_level_1_approval', 'infs_purchasing.purchase_supervisor_level_1')
+                elif stage == 'approved_lvl_1':
+                    record._send_approval_email('infs_purchasing.email_template_rfqgp_level_2_approval', 'infs_purchasing.purchase_supervisor_level_2')
+                elif stage == 'approved_lvl_2':
+                    record._send_approval_email('infs_purchasing.email_template_rfqgp_level_3_approval', 'infs_purchasing.purchase_supervisor_level_3')
+                elif stage == 'approved':
+                    record.action_send_confirmed_mail_to_rfqgp()
+                    _logger.info(f"Final approval email sent to the purchase order owner.")
+            else:
+                _logger.info(f"Approval mails are disabled. Not sending notification for stage: {stage}")
+
+            
+
+    def _send_approval_email(self, template_xml_id, mail_to_group):
         send_approval_mails = self.check_approval_mail_status()
-        _logger.info(f"send_approval_mails: {send_approval_mails}")
         if send_approval_mails:
-            _logger.info(f"Sending approval notification for stage: {stage}")
-            if stage == 'submitted':
-                self._send_approval_email('infs_purchasing.email_template_rfqgp_level_1_approval', 'infs_purchasing.purchase_supervisor_level_1')
-            elif stage == 'approved_lvl_1':
-                self._send_approval_email('infs_purchasing.email_template_rfqgp_level_2_approval', 'infs_purchasing.purchase_supervisor_level_2')
-            elif stage == 'approved_lvl_2':
-                self._send_approval_email('infs_purchasing.email_template_rfqgp_level_3_approval', 'infs_purchasing.purchase_supervisor_level_3')
-            elif stage == 'approved':
-                self.action_send_confirmed_mail_to_rfqgp()
-                _logger.info(f"Final approval email sent to the purchase order owner.")
+            mail_servers = self.env['ir.mail_server'].sudo().search([], limit=1)
+            # _logger.info(f"Outgoing mail server found: {mail_servers[0].name}")
+            if not mail_servers:
+                _logger.warning("No outgoing mail server configured. Skipping email sending.")
+                return
+            else:
+                _logger.info(f"Not Skipping?")
+                template = self.env.ref(template_xml_id)
+                email_to = self.get_email_to(mail_to_group)
+                if email_to:
+                    # template.write({'email_to': email_to,'email_from': 'Infinity IT Group - CRM <crm@infinityitsuccess.com>',})
+                    template.write({'email_to': email_to,'email_from': 'Min Pyae Sone - adMIN <minpyaesone.dev@gmail.com>',})
+                    for order in self:
+                        _logger.info(f"Attempting to send testing email for{order.name}.")
+                        _logger.info(f"This is self {self.id}.")
+                    template.send_mail(self.id, force_send=True, raise_exception=True)
+                else:
+                    _logger.warning("No email addresses found for sending approval email.")
         else:
-            _logger.info(f"Approval mails are disabled. Not sending notification for stage: {stage}")
+            _logger.info(f"Approval mails are disabled. Not sending email.")
+
+    @api.model
+    def get_email_to(self,mail_to_group):
+        user_group = self.env.ref(mail_to_group)        
+        email_list = [
+            usr.partner_id.email for usr in user_group.users if usr.partner_id.email
+        ]
+        return ",".join(email_list) 
             
     def action_send_confirmed_mail_to_rfqgp(self):
         _logger.info(f"Preparing to send confirmed email to RFQ Group owner for RFQ Group {self.name}.")
@@ -199,8 +241,8 @@ class PurchaseOrderGroupInherit(models.Model):
                 try:
                     if send_approval_mails:
                         _logger.info(f"Attempting to send email to {order.create_uid.partner_id.email} for RFQ Group {order.name}.")         
-                        template.write({'email_to': email_to,'email_from': 'Infinity IT Group - CRM <crm@infinityitsuccess.com>',})   
-                        # template.write({'email_to': email_to,'email_from': 'Min Pyae Sone - adMIN <minpyaesone.dev@gmail.com>',})
+                        # template.write({'email_to': email_to,'email_from': 'Infinity IT Group - CRM <crm@infinityitsuccess.com>',})   
+                        template.write({'email_to': email_to,'email_from': 'Min Pyae Sone - adMIN <minpyaesone.dev@gmail.com>',})
                         template.send_mail(order.id, force_send=True, raise_exception=True)
                         _logger.info(f"Email successfully sent to {order.create_uid.partner_id.email}.")
                     else:
